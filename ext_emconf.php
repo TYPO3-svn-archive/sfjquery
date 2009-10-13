afb355fbfeb791c530a9f87baabeb9d8c17786f8
froemken <froemken@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-10-13 07:31:03 +0000

    first upload
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sfjquery/trunk@25342 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,48 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "sfjquery"
+#
+# Auto generated 30-09-2009 12:48
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'SF jQuery',
+	'description' => 'This is an interface between jQuery and TYPO3. Make use of TS and HTML for templating. Add CSS. So programm what ever you want.',
+	'category' => 'plugin',
+	'shy' => 0,
+	'version' => '1.0.0',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Stefan Froemken',
+	'author_email' => 'firma@sfroemken.de',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:20:{s:9:"ChangeLog";s:4:"fd31";s:10:"README.txt";s:4:"ee2d";s:21:"ext_conf_template.txt";s:4:"c2ce";s:12:"ext_icon.gif";s:4:"5088";s:17:"ext_localconf.php";s:4:"1816";s:14:"ext_tables.php";s:4:"e6b1";s:13:"locallang.xml";s:4:"61b2";s:16:"locallang_db.xml";s:4:"a8d1";s:14:"doc/manual.sxw";s:4:"a4f5";s:19:"doc/wizard_form.dat";s:4:"dd75";s:20:"doc/wizard_form.html";s:4:"d0ba";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:29:"pi1/class.tx_sfjquery_pi1.php";s:4:"4264";s:37:"pi1/class.tx_sfjquery_pi1_wizicon.php";s:4:"51db";s:13:"pi1/clear.gif";s:4:"cc11";s:19:"pi1/flexform_ds.xml";s:4:"a0c5";s:17:"pi1/locallang.xml";s:4:"58b1";s:23:"res/jquery-1.3.2.min.js";s:4:"7d91";s:33:"res/jquery-ui-1.7.2.custom.min.js";s:4:"8854";s:21:"res/pi1_template.html";s:4:"84c3";}',
+	'suggests' => array(
+	),
+);
+
+?>
