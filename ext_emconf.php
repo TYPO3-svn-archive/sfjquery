5d5c03c3e4ebc384023aa3bbfc47d84760b429c4
froemken <froemken@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-01-07 11:25:12 +0000

    Now you can add external files to all of your codingboxes. This is very helpful within big project or if you want to code in your own software. Further you can use sfjquery as a plain template machine if you want it. Some Bugfixes and Code cleanings.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sfjquery/trunk@28479 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "sfjquery".
 #
-# Auto generated 20-10-2009 15:53
+# Auto generated 07-01-2010 12:12
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'This is an interface between jQuery and TYPO3. Make use of TS and HTML for templating. Add CSS. So programm what ever you want.',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '1.1.2',
+	'version' => '1.1.3',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:22:{s:9:"ChangeLog";s:4:"a1e3";s:10:"README.txt";s:4:"ee2d";s:21:"ext_conf_template.txt";s:4:"c2ce";s:12:"ext_icon.gif";s:4:"5088";s:17:"ext_localconf.php";s:4:"89dd";s:14:"ext_tables.php";s:4:"e6b1";s:13:"locallang.xml";s:4:"61b2";s:16:"locallang_db.xml";s:4:"a8d1";s:14:"doc/manual.sxw";s:4:"4521";s:19:"doc/wizard_form.dat";s:4:"dd75";s:20:"doc/wizard_form.html";s:4:"d0ba";s:35:"hooks/class.tx_sfjquery_otherjs.php";s:4:"6cb8";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:29:"pi1/class.tx_sfjquery_pi1.php";s:4:"8613";s:37:"pi1/class.tx_sfjquery_pi1_wizicon.php";s:4:"51db";s:13:"pi1/clear.gif";s:4:"cc11";s:19:"pi1/flexform_ds.xml";s:4:"a0c5";s:17:"pi1/locallang.xml";s:4:"58b1";s:17:"res/ajaxupload.js";s:4:"a205";s:23:"res/jquery-1.3.2.min.js";s:4:"7d91";s:33:"res/jquery-ui-1.7.2.custom.min.js";s:4:"8854";s:21:"res/pi1_template.html";s:4:"c772";}',
+	'_md5_values_when_last_written' => 'a:23:{s:9:"ChangeLog";s:4:"a1e3";s:10:"README.txt";s:4:"ee2d";s:21:"ext_conf_template.txt";s:4:"cb20";s:12:"ext_icon.gif";s:4:"5088";s:17:"ext_localconf.php";s:4:"3d4f";s:14:"ext_tables.php";s:4:"e6b1";s:13:"locallang.xml";s:4:"61b2";s:16:"locallang_db.xml";s:4:"da03";s:16:"ajax/contact.php";s:4:"5875";s:14:"doc/manual.sxw";s:4:"bdbc";s:19:"doc/wizard_form.dat";s:4:"dd75";s:20:"doc/wizard_form.html";s:4:"d0ba";s:35:"hooks/class.tx_sfjquery_otherjs.php";s:4:"6cb8";s:14:"pi1/ce_wiz.gif";s:4:"02b6";s:29:"pi1/class.tx_sfjquery_pi1.php";s:4:"ea9c";s:37:"pi1/class.tx_sfjquery_pi1_wizicon.php";s:4:"51db";s:13:"pi1/clear.gif";s:4:"cc11";s:19:"pi1/flexform_ds.xml";s:4:"b4e2";s:17:"pi1/locallang.xml";s:4:"58b1";s:17:"res/ajaxupload.js";s:4:"a205";s:23:"res/jquery-1.3.2.min.js";s:4:"7d91";s:33:"res/jquery-ui-1.7.2.custom.min.js";s:4:"8854";s:21:"res/pi1_template.html";s:4:"56d9";}',
 	'suggests' => array(
 	),
 );
--- ext_localconf.php
+++ ext_localconf.php
@@ -5,6 +5,8 @@ if (!defined ('TYPO3_MODE')) {
 
 t3lib_extMgm::addPItoST43($_EXTKEY, 'pi1/class.tx_sfjquery_pi1.php', '_pi1', 'list_type', 1);
 
+$TYPO3_CONF_VARS['FE']['eID_include']['sfjquery-contact'] = 'EXT:sfjquery/ajax/contact.php';
+
 $extConf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['sfjquery']);
 if($extConf['addPluginAjaxupload']) {
 	$TYPO3_CONF_VARS['EXTCONF']['sfjquery']['otherJS'][] = 'EXT:'.$_EXTKEY.'/hooks/class.tx_sfjquery_otherjs.php:tx_sfjquery_otherjs->additionalJS';
